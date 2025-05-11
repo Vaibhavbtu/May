6518dc449d34333b0dedeff19be81fb469bd4fe0
Vaibhavbtu <148687341+Vaibhavbtu@users.noreply.github.com>
2025-05-11 22:50:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-ijbadhdgvhjgdsvjhgdsjhvgdv
-dvhdsjvhgsdhjvgdsvsd
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('name', 'Jane Random', 'Your name.')
+
+def main(argv):
+  if FLAGS.debug:
+    print('non-flag arguments:', argv)
+  print('Happy, ', FLAGS.name)
+
+
+if __name__ == '__main__':
+  app.run(main)
